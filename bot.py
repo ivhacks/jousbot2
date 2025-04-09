eb162fe2a345d2de7b9a1b8f97e956d6d3bd4ddb
Iv Robinson <jousboxx@gmail.com>
2025-04-09 05:40:09 +0000

    python working

---
+++ bot.py
@@ -0,0 +1,20 @@
+import discord
+
+with open('.token', 'r') as f:
+    TOKEN = f.read().strip()
+
+intents = discord.Intents.default()
+intents.message_content = True
+bot = discord.Client(intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f'{bot.user} has connected to Discord!')
+
+@bot.event
+async def on_message(message):
+    # Check if the message is a DM and not from the bot itself
+    if isinstance(message.channel, discord.DMChannel) and message.author != bot.user:
+        await message.channel.send("hi")
+
+bot.run(TOKEN)
