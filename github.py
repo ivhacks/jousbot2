df6c6f19fb30e4f9556b5a77e402ba27a120b933
Iv Robinson <jousboxx@gmail.com>
2025-04-10 04:32:11 +0000

    can delete repository

--- github.py
+++ github.py
@@ -17,7 +17,24 @@ def create_repository(name: str, description: str):
     if response.status_code == 201:
         return
     else:
-        raise Exception(f"Failed to create repository. Status code: {response.status_code}. Error: {response.json()}")
+        raise Exception(
+            f"Failed to create repository. Status code: {response.status_code}. Error: {response.json()}"
+        )
 
 
-create_repository("balls", "Created via GitHub API")
+def delete_repository(name: str):
+    response = requests.delete(
+        f"https://api.github.com/repos/{config['github_username']}/{name}",
+        headers=HEADERS,
+    )
+
+    if response.status_code == 204:
+        return
+    else:
+        raise Exception(
+            f"Failed to delete repository. Status code: {response.status_code}. Error: {response.json()}"
+        )
+
+
+# create_repository("balls", "Created via GitHub API")
+delete_repository("balls")
